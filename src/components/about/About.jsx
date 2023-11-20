1a169006b1a85b9c4274bbf4d4ef397835ab5860
seegunz <seegn8@gmail.com>
2023-11-20 03:04:53 -0600

    added the post for firebase already

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './about.css'
 
 export default function About() {
+    const [input, setInput] = useState('')
+    const [text, setText] = useState([])
+    const [query, setQuery] = useState('')
+
+    const filtered = text.filter(list =>{
+        return list.toLowerCase().includes(query.toLowerCase())
+    })
+
+    function handleChange(e) {
+        setQuery(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setText(prev=> {
+            return [...prev, input]
+        })
+        setInput('')
+    }
   return (
-    <div>
-        about
+    <div className='about'>
+        <h1>search items</h1>
+        <input type="search" onChange={handleChange}/> <br /><br /><br />
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={input} onChange={e=>setInput(e.target.value)}/>
+            <button>submit</button>
+        </form>
+        {filtered.map(item=><div>{item} </div>)}
     </div>
   )
 }
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './profile.css'
-import {auth} from '../auth/firebase'
+import { db } from '../auth/firebase'
 import { useContext } from 'react'
 import { AppContext } from '../../App'
+import { addDoc, collection, getDocs } from 'firebase/firestore'
 
 
 export default function Profile() {
     const {user} = useContext(AppContext)
+    const [input, setInput] = useState('')
+    const [list, setList] = useState([])
+
+    const value = collection(db, "post")
+
+    useEffect(()=>{
+      async function getData() {
+        const dbVal = await getDocs(value)
+        setList(dbVal.docs.map(doc=>({...doc.data(), id:doc.id})))
+      }
+      getData()
+    })
+
+    async function handleSubmit(e) {
+      e.preventDefault()
+      await addDoc(value, {addPost:input})
+
+      // setList(prev=>{
+      //   return ([...prev, input])
+      // })
+       setInput('')
+    }
     
   return (
-    <div>
-      profile {user?.displayName}
+    <div className='profile'>
+      <h1>Welcome {user?.displayName}</h1>
+      <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='Add Post' value={input} onChange={e=>setInput(e.target.value)}/>
+          {/* <input type="file" placeholder='Add Post' value={input} onChange={e=>setInput(e.target.value)}/> */}
+          <button type="submit">Add Post</button>
+      </form>
+      <div className='post'>
+          {list.sort().map((items, i) => <p key={i}>{items.addPost} </p>)}
+      </div>
     </div>
   )
 }
